1deb89e116ba27cff90b9a62403e759b2ac36d9d
Mars <abdullah.sid786@gmail.com>
2022-09-05 23:06:08 +0530

    add sidebar top

---
+++ src/Chats.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Chats() {
+  return (
+    // <div>I am Chats</div>
+    <></>
+  )
+}
+
+export default Chats
---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import "./features/Sidebar.css"
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h1>Mars</h1>
+      </div>
+      <ArrowDropDownIcon />
+    </div>
+  )
+}
+
+export default Sidebar
