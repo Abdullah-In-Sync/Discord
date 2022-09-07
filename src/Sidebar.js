f633902b13228004a5ddbb4e53cf16ed9b664669
Mars <abdullah.sid786@gmail.com>
2022-09-07 23:12:07 +0530

    sidebar #

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import "./features/Sidebar.css"
+import { SidebarChannel } from './SidebarChannel';
 
 function Sidebar() {
   return (
@@ -19,6 +20,12 @@ function Sidebar() {
           </div>
           <AddIcon className="sidebar__addChannel" />
         </div>
+      <div className="sidebar__channelsList">
+        <SidebarChannel />
+        <SidebarChannel />
+        <SidebarChannel />
+        <SidebarChannel />
+      </div>
       </div>
     </div>
   )
---
+++ src/SidebarChannel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./features/SidebarChannel.css"
+
+export const SidebarChannel = () => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannel__hash">#</span>Youtube
+      </h4>
+    </div>
+  )
+}
