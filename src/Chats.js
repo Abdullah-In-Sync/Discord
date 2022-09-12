833b071d3e97a335f60079a96d0b05f8d7a3fd00
abdullah <abdullah.raghib@vidyamantra.com>
2022-09-12 15:09:06 +0530

    chats input icon done

--- src/Chats.js
+++ src/Chats.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { ChatsHeader } from './ChatsHeader'
 import "./features/Chat.css"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 function Chats() {
   return (
     <div className="chats">
       <ChatsHeader />
+      <div className="chats__messages">
+
+      </div>
+      <div className="chats__input">
+        <AddCircleIcon fontSize="large" />
+        <form action="">
+          <input type="text" placeholder='{Message #MyChannel}' />
+          <button className='chats__inputButton' type='submit'> Send Message</button>
+        </form>
+        <div className="chats__inputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
     
   )
