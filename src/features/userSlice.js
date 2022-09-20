dedc0d8a143dc498b9bb72bdbc429a6570bf8756
Mars <abdullah.sid786@gmail.com>
2022-09-20 22:03:04 +0530

    firebase set

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,9 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from '../features/counterAPI';
+import { fetchCount } from './counterAPI';
 
 const initialState = {
-  value: 0,
-  status: 'idle',
+  user: null,
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -20,8 +19,8 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const userSlice = createSlice({
+  name: 'user',
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
@@ -54,12 +53,12 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.user.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
@@ -70,4 +69,4 @@ export const incrementIfOdd = (amount) => (dispatch, getState) => {
   }
 };
 
-export default counterSlice.reducer;
+export default userSlice.reducer;
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAWDqzeNhFLE17KAJByR9rXFB7c8v0RTl0",
   authDomain: "discord-mars.firebaseapp.com",
@@ -18,10 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-const db = app.firestore();
-const auth = app.auth();
-const provider = new app.auth.GoogleAuthProvider();
-console.log(db, auth, provider);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
 export { auth, provider};
 export default db;
