7c098b8c8d50ec4d3c4f59067e08391930a1b768
Mars <abdullah.sid786@gmail.com>
2022-09-27 23:10:30 +0530

    message not showing

--- src/Chats.js
+++ src/Chats.js
@@ -1,35 +1,67 @@
-import React from 'react'
-import { ChatsHeader } from './ChatsHeader'
-import "./features/Chat.css"
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
-import GifIcon from '@mui/icons-material/Gif';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
-import { Messages } from './Messages';
-import { useSelector } from 'react-redux';
-import { selectChannelId, selectChannelName } from './features/appSlice';
-import { selectUser } from './features/userSlice';
-
-
-
+import React, { useEffect, useState } from "react";
+import { ChatsHeader } from "./ChatsHeader";
+import "./features/Chat.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import GifIcon from "@mui/icons-material/Gif";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import { Messages } from "./Messages";
+import { useSelector } from "react-redux";
+import { selectChannelId, selectChannelName } from "./features/appSlice";
+import { selectUser } from "./features/userSlice";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "@firebase/firestore";
+import db from "./firebase";
 
 function Chats() {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+  const [messeges, setMessages] = useState([]);
+console.log("channelId", channelId);
+  const q = query(collection(db, "channels"), orderBy("timeStamp"));
+  useEffect(() => {
+    if (channelId) {
+      onSnapshot(q, (docs) => {
+        // setMessages(docs.data());
+        console.log("data");
+      });
+    }
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    addDoc(q, {
+      timestamp: serverTimestamp(),
+      message: input,
+      user: user,
+    });
+    setInput("");
+  };
   return (
     <div className="chats">
       <ChatsHeader channelName={channelName} />
       <div className="chats__messages">
-        <Messages />
-        <Messages />
-        <Messages />
+        {messeges.map((message) => (
+          <Messages />
+        ))}
       </div>
       <div className="chats__input">
         <AddCircleIcon fontSize="large" />
         <form action="">
-          <input type="text" placeholder='{Message #MyChannel}' />
-          <button className='chats__inputButton' type='submit'> Send Message</button>
+          <input
+            type="text"
+            value={(e) => setInput(e.target.value)}
+            placeholder={`Message # ${channelName}`}
+          />
+          <button
+            onClick={sendMessage}
+            className="chats__inputButton"
+            type="submit"
+          >
+            {" "}
+            Send Message
+          </button>
         </form>
         <div className="chats__inputIcons">
           <CardGiftcardIcon />
@@ -38,8 +70,7 @@ function Chats() {
         </div>
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default Chats
+export default Chats;
